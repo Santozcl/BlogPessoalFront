70b4772a99c6ec8c38aaef905a5906c347cd4ebc
Santozcl <carloseduardodossantossilva111@gmail.com>
2022-11-22 16:52:27 -0300

    exe

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Title } from './componentes/Title/Title';
+import { Text } from './componentes/text/Text';
 import './App.css';
 
 function App() {
+  const nome = 'Diogo'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello {nome}!</h1>
+    <p>
+      teste de texto de paragrafo
+    </p>
+    </>
   );
 }
-
+    
 export default App;
---
+++ src/componentes/text/Text.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import'./Text.css';
+
+function Text(){
+    return(
+        <p>Teste</p>
+    )
+}
+export{Text}
